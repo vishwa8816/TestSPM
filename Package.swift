958318eefadbe40a5c6c491e76f2f4c5c9c338d8
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-05 23:22:35 +1000

    Test SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TestSPM",
+    platforms: [
+        .iOS(.v15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,17 +15,22 @@ let package = Package(
             targets: ["TestSPM"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TestSPM",
-            dependencies: []),
+            dependencies: [
+                .target(name: "Test"),
+                .product(name: "Alamofire", package: "Alamofire"),
+            ]),
         .testTarget(
             name: "TestSPMTests",
             dependencies: ["TestSPM"]),
+        .binaryTarget(name: "Test",
+                      url: "https://github.com/vishwa8816/MyTestFrameworkA/releases/download/Fourth/Test.xcframework.zip",
+                      checksum: "48b2da9cb452b4772a15166775401eb66b9f9d961ba7377029d3b65de8d64f91")
     ]
 )
